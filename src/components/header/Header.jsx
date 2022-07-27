82ee3b616363daf15fe4a83a1fed02d57601bcdd
piyush7827 <piyushshukla7193@gmail.com>
2022-07-27 16:35:55 +0530

    api integration

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CButton } from "@coreui/react";
 import "./header.css";
 
-const Header = () => {
+const Header = (props) => {
+  const { filterMoviesBySearch } = props;
   const navigate = useNavigate();
+  const [searchText, setSearchText] = useState("");
 
   const logoutFn = () => {
     localStorage.clear();
@@ -13,6 +16,11 @@ const Header = () => {
 
   const loginFn = () => {
     navigate("/login");
+  };
+  const searchFn = (e) => {
+    console.log(searchText);
+    e.preventDefault();
+    filterMoviesBySearch(searchText);
   };
 
   const isUserLoggedIn = localStorage.getItem("accessToken");
@@ -30,6 +38,21 @@ const Header = () => {
           MY TICKET
         </a>
       </div>
+
+      <form className="d-flex" onSubmit={searchFn}>
+        <input
+          type="text"
+          className="custom-input"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+          placeholder={"Enter movie name"}
+        />
+        <CButton type="submit" color="danger" className="px-3 searchBtn">
+          Search
+        </CButton>
+      </form>
 
       {isUserLoggedIn ? (
         <CButton
